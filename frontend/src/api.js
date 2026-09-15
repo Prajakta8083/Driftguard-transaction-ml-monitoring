09718e0c950a91ed147fbd671378630d56213caf
Prjakta8083 <prajaktazanzane3@gmail.com>
2026-09-15 20:01:05 +0530

    Connect frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://driftguard-backend-xok2.onrender.com";
 
 async function request(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
